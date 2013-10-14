cad6894a57183bb3b22fa5ea1445c0c9732a233a
Gao Yuan <gao_303407@163.com>
2013-10-14 17:16:13 +0800

    First three chapters

---
+++ HeadFirst/src/ChapterThree/CompilerA.java
@@ -0,0 +1,32 @@
+package ChapterThree;
+
+public class CompilerA {
+    static class Books {
+        String title;
+        String author;
+    }
+
+    static class BooksTestDrive {
+        public static void main(String[] args) {
+            Books[] myBooks = new Books[3];
+            int x = 0;
+
+            myBooks[0] = new Books();
+            myBooks[1] = new Books();
+            myBooks[2] = new Books();
+            myBooks[0].title = "The Grapes of Jave";
+            myBooks[1].title = "The Jave Gatsby";
+            myBooks[2].title = "The Jave Cookbook";
+            myBooks[0].author = "bob";
+            myBooks[1].author = "sue";
+            myBooks[2].author = "ian";
+
+            while (x < 3) {
+                System.out.println(myBooks[x].title);
+                System.out.println(" by ");
+                System.out.println(myBooks[x].author);
+                x += 1;
+            }
+        }
+    }
+}
